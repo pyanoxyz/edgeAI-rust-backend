1122856ee0457c5b7517bba01ad7dba95a0ec9c9
CJ <devcj.in@gmail.com>
2024-10-08 07:04:24 +0530

    minor fixes

--- run-server.sh
+++ run-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set the DYLD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH="$HOME/.pyano/binaries/lib:$DYLD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$HOME/.pyano/bin/libtorch/lib:$DYLD_LIBRARY_PATH"
 
 # Run the Rust binary (replace `target/debug/your_binary` with your actual binary path)
-exec target/aarch64-apple-darwin/release/pyano_server
+exec $HOME/.pyano/pyano-server
 
